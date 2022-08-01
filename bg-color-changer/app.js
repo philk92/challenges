032affbe89653e59bd01728d0ec1d9912a6da221
philk92 <philkienzle@gmail.com>
2022-08-01 18:25:14 +0200

    Fixed with, grey button

--- bg-color-changer/app.js
+++ bg-color-changer/app.js
@@ -3,24 +3,19 @@ const button = document.querySelector("button");
 const h1 = document.querySelector("h1");
 
 function randCol() {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
   const a = Math.floor(Math.random() * 6) / 10 + 0.5;
   return `rgba(${r},${g},${b},${a})`;
 }
 
 button.addEventListener("click", () => {
-  body.style.backgroundColor = randCol(0.5);
+  body.style.backgroundColor = randCol();
   h1.innerText = body.style.backgroundColor;
 });
 
 function onload() {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
-  const a = Math.floor(Math.random() * 6) / 10 + 0.5;
-  let str = `rgba(${r},${g},${b},${a})`;
-  body.style.backgroundColor = str;
+  body.style.backgroundColor = randCol();
   h1.innerText = body.style.backgroundColor;
 }
