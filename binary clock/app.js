e5c9a0e0d29f76df702f0ec2c692905a3e4be569
philk92 <philkienzle@gmail.com>
2022-08-08 07:12:42 +0200

    Refactor code and change visual

--- binary clock/app.js
+++ binary clock/app.js
@@ -1,5 +1,5 @@
+let dots = document.querySelectorAll(".dot");
 function time() {
-  let dots = document.querySelectorAll(".dot");
   for (const dot of dots) {
     dot.classList.remove("on");
   }
@@ -11,60 +11,50 @@ function time() {
   minute = minute < 10 ? "0" + minute : minute;
   second = second < 10 ? "0" + second : second;
 
-  let hourString = Number(hour).toString().split("");
-  let minuteString = Number(minute).toString().split("");
-  let secondString = Number(second).toString().split("");
-  if (hourString.length === 1) {
-    hourString.unshift("0");
-  }
-  if (minuteString.length === 1) {
-    minuteString.unshift("0");
-  }
-  if (secondString.length === 1) {
-    secondString.unshift("0");
-  }
-  toggleClass(hourString[0], "hour1");
-  toggleClass(hourString[1], "hour2");
-  toggleClass(minuteString[0], "min1");
-  toggleClass(minuteString[1], "min2");
-  toggleClass(secondString[0], "sec1");
-  toggleClass(secondString[1], "sec2");
+  toggleClass(hour.toString()[0], "hour1");
+  toggleClass(hour.toString()[1], "hour2");
+  toggleClass(minute.toString()[0], "min1");
+  toggleClass(minute.toString()[1], "min2");
+  toggleClass(second.toString()[0], "sec1");
+  toggleClass(second.toString()[1], "sec2");
   setInterval(time, 1000);
 }
 
 function toggleClass(string, classname) {
-  if (string === "1") {
-    document.getElementById(`${classname}1`).classList.add("on");
-  }
-  if (string === "2") {
-    document.getElementById(`${classname}2`).classList.add("on");
-  }
-  if (string === "3") {
-    document.getElementById(`${classname}1`).classList.add("on");
-    document.getElementById(`${classname}2`).classList.add("on");
-  }
-  if (string === "4") {
-    document.getElementById(`${classname}4`).classList.add("on");
-  }
-  if (string === "5") {
-    document.getElementById(`${classname}1`).classList.add("on");
-    document.getElementById(`${classname}4`).classList.add("on");
-  }
-  if (string === "6") {
-    document.getElementById(`${classname}2`).classList.add("on");
-    document.getElementById(`${classname}4`).classList.add("on");
-  }
-  if (string === "7") {
-    document.getElementById(`${classname}1`).classList.add("on");
-    document.getElementById(`${classname}2`).classList.add("on");
-    document.getElementById(`${classname}4`).classList.add("on");
-  }
-  if (string === "8") {
-    document.getElementById(`${classname}8`).classList.add("on");
-  }
-  if (string === "9") {
-    document.getElementById(`${classname}1`).classList.add("on");
-    document.getElementById(`${classname}8`).classList.add("on");
+  switch (string) {
+    case "1":
+      document.getElementById(`${classname}1`).classList.add("on");
+      break;
+    case "2":
+      document.getElementById(`${classname}2`).classList.add("on");
+      break;
+    case "3":
+      document.getElementById(`${classname}1`).classList.add("on");
+      document.getElementById(`${classname}2`).classList.add("on");
+      break;
+    case "4":
+      document.getElementById(`${classname}4`).classList.add("on");
+      break;
+    case "5":
+      document.getElementById(`${classname}1`).classList.add("on");
+      document.getElementById(`${classname}4`).classList.add("on");
+      break;
+    case "6":
+      document.getElementById(`${classname}2`).classList.add("on");
+      document.getElementById(`${classname}4`).classList.add("on");
+      break;
+    case "7":
+      document.getElementById(`${classname}1`).classList.add("on");
+      document.getElementById(`${classname}2`).classList.add("on");
+      document.getElementById(`${classname}4`).classList.add("on");
+      break;
+    case "8":
+      document.getElementById(`${classname}8`).classList.add("on");
+      break;
+    case "9":
+      document.getElementById(`${classname}1`).classList.add("on");
+      document.getElementById(`${classname}8`).classList.add("on");
+      break;
   }
 }
 
